092f0ce95ffebe069016d27b4b37f099a46b685e
JBgoterps <153498315+JBgoterps@users.noreply.github.com>
2023-12-11 18:16:06 -0500

    Third Commit - Minor Line Change

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ wr_allrows = wr_soup.find("table", {"id":"rec_yds_leaders"}).find("tbody").find_
 # Shrinks table to first 10 rows
 wr_rows = wr_allrows[:10]
 
-# Gets rid of extra characters to make final product cleaner
+# Gets rid of extra characters to make final product cleaner, and puts data into array
 players = np.array([row.find_all("td")[0].text.replace("+", "") for row in wr_rows])
 yards = np.array([int(row.find_all("td")[1].text.replace(",", "")) for row in wr_rows])
 
